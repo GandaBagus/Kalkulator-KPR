576dd7d955c30faa6f25dd3b4b91ed73f90dcbb4
GandaBagus <gandabaguswibisono123@gmail.com>
2025-03-17 16:24:04 +0700

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Ganda Bagus Wibisono on 17/03/25.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
