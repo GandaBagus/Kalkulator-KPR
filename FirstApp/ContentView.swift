f8b0cf0b02b5f491996e7df4b39b1e1b37f0c8c6
GandaBagus <gandabaguswibisono123@gmail.com>
2025-03-17 19:23:22 +0700

    Update ContentView.swift

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -6,16 +6,69 @@
 //
 
 import SwiftUI
-
+ 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        Text("Masukkan Data")
+            .fontWeight(.black)
+            .font(.largeTitle)
+        
+            
+            
+            
+        GroupBox() {
+            Label {
+                Text("Harga Rumah")
+            } icon: {
+               // Image(systemName: "ruler")
+            }
+            .padding(.leading, -180.0)
+            HStack{
+                TextField("Masukkan harga rumah", text: .constant("")).textFieldStyle(.roundedBorder)
+                Text("Rp      ")
+            }
+            
+            
+            Label {
+                Text("Bunga")
+            } icon: {
+                //Image(systemName: "ruler")
+            }
+            .padding(.leading, -180.0)
+            HStack{
+                TextField("Masukkan besaran bunga", text: .constant("")).textFieldStyle(.roundedBorder)
+                Text("%        ")
+            }
+            
+            
+            Label {
+                Text("Tenor")
+            } icon: {
+                //Image(systemName: "ruler")
+            }
+            .padding(.leading, -180.0)
+            HStack{
+                TextField("Masukkan jumlah tenor", text: .constant("")).textFieldStyle(.roundedBorder)
+                Text("Bulan  ")
+            }
+            
         }
-        .padding()
+        .padding([.top, .leading, .trailing], 0.0)
+        
+        HStack{
+            Button("Hitung") {
+                
+            }
+            //Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                
+            //}
+        }
+        .padding(.top)
+        .buttonStyle(.bordered)
+        
+       
+
     }
 }
 
